a5b4ae7b3b994e7c12d1956d04410fca15a8a381
Soshi Katsuta <soshi.katsuta@gmail.com>
2015-09-02 01:30:06 +0900

    04: implement Mult.asm
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// -- pseudo code --
+// R2 = 0;
+// while (R1 > 0) {
+//     R2 = R2 + R0;
+//     R1--;
+// }
+
+    @R2 // init R2 = 0
+    M=0
+(LOOP)
+    @R1 // check if R1 > 0
+    D=M
+    @END
+    D;JLE
+    @R0 // add R2 = R2 + R0
+    D=M
+    @R2
+    M=M+D
+    @R1 // decrement R1--
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
