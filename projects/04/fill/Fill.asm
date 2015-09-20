4452accbd83305aa51fc94af3476daa5abf1f860
Soshi Katsuta <soshi.katsuta@gmail.com>
2015-09-20 23:28:10 +0900

    04: implement Fill.asm
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,71 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+// define max address in a screen
+@24576
+D=A
+@MAXSCREEN
+M=D
+
+// define screen pointer
+@SCREEN
+D=A
+@POINTER
+M=D
+
+// infinite loop
+(LOOP)
+    // jump to FILL if keyboard input
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+
+    // otherwise jump to UNFILL
+    @UNFILL
+    0;JMP
+
+(UNFILL)
+    // do nothing if POINTER == SCREEN
+    @SCREEN
+    D=A
+    @POINTER
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // unfill the screen
+    @POINTER
+    A=M
+    M=0
+
+    // decrement POINTER
+    @POINTER
+    M=M-1
+
+    // jump back to main loop
+    @LOOP
+    0;JMP
+
+(FILL)
+    // do nothing if the screen is full
+    @MAXSCREEN
+    D=M
+    @POINTER
+    D=D-M
+    @LOOP
+    D;JEQ
+
+    // fill in the pixel that POINTER points to
+    @POINTER
+    A=M
+    M=-1 // -1 == 0xFFFF (all 1)
+
+    // iterate pointer by 1
+    @POINTER
+    M=M+1
+
+    // jump back to main loop
+    @LOOP
+    0;JMP
