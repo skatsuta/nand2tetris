03936219f0766aecb25682f642005b5beec36ad7
Soshi Katsuta <soshi.katsuta@gmail.com>
2015-09-22 02:10:58 +0900

    06: add parser struct and hasMoreCommands() method
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

---
+++ projects/06/assembler/parser/parser.go
@@ -0,0 +1,41 @@
+package parser
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+const (
+	// prefixComment is a prefix of a comment.
+	prefixComment = "//"
+)
+
+// parser is a parser for Hack assembly language.
+type parser struct {
+	in *bufio.Scanner
+}
+
+// newParser creates a new parser object that reads and parses r.
+func newParser(r io.Reader) *parser {
+	return &parser{
+		in: bufio.NewScanner(r),
+	}
+}
+
+// hasMoreCommands reports whether there exist more commands in input.
+func (p *parser) hasMoreCommands() bool {
+	// if Scan() == true && Text() is a comment, continue (skip)
+	// if Scan() == true && Text() is not a comment, return true
+	// if Scan() == false, return false
+	for p.in.Scan() {
+		// trim all leading and trailing white spaces
+		line := strings.TrimSpace(p.in.Text())
+
+		// return true if the line is not empty and not a comment, that is, a command
+		if line != "" && !strings.HasPrefix(line, prefixComment) {
+			return true
+		}
+	}
+	return false
+}
---
+++ projects/06/assembler/parser/parser_test.go
@@ -0,0 +1,56 @@
+package parser
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+var testAsm = `
+
+// This is a comment.
+@16
+
+// This is also a comment.
+D=M
+
+    @17 // indent spaces
+	D=A // indent tab
+`
+
+func TestNewParser(t *testing.T) {
+	filename := "../../add/Add.asm"
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatalf("failed to open %s: %s", filename, err.Error())
+	}
+
+	newParserTests := []struct {
+		r io.Reader
+	}{
+		{strings.NewReader(testAsm)},
+		{file},
+	}
+
+	for _, tt := range newParserTests {
+		got := newParser(tt.r)
+		if got.in == nil {
+			t.Errorf("input is nil")
+		}
+	}
+}
+
+func TestHasMoreCommands(t *testing.T) {
+	a := newParser(strings.NewReader(testAsm))
+	numOfCmdsInTestAsm := 4
+	var cnt int
+
+	for a.hasMoreCommands() {
+		cnt++
+	}
+
+	if cnt != numOfCmdsInTestAsm {
+		t.Errorf("# of commands in testAsm should be %d, but got %d", numOfCmdsInTestAsm, cnt)
+	}
+}
