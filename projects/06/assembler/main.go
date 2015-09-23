66e97d727ab111fd5969ba574d60e351dd05f2f3
Soshi Katsuta <soshi.katsuta@gmail.com>
2015-09-24 00:06:04 +0900

    06/assembler/main: add pre-defined symbols
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- projects/06/assembler/main.go
+++ projects/06/assembler/main.go
@@ -11,8 +11,36 @@ import (
 )
 
 const (
+	// extension name of binary file
 	binExt = "hack"
 )
+
+// pre-defined symbols
+var preDefSymb = map[string]uintptr{
+	"SP":     0x0,
+	"LCL":    0x1,
+	"ARG":    0x2,
+	"THIS":   0x3,
+	"THAT":   0x4,
+	"R0":     0x0,
+	"R1":     0x1,
+	"R2":     0x2,
+	"R3":     0x3,
+	"R4":     0x4,
+	"R5":     0x5,
+	"R6":     0x6,
+	"R7":     0x7,
+	"R8":     0x8,
+	"R9":     0x9,
+	"R10":    0xA,
+	"R11":    0xB,
+	"R12":    0xC,
+	"R13":    0xD,
+	"R14":    0xE,
+	"R15":    0xF,
+	"SCREEN": 0x4000,
+	"KBD":    0x6000,
+}
 
 func main() {
 	flag.Parse()
@@ -45,22 +73,29 @@ func main() {
 // convert converts `in` source assembly code to machine code and write it to `out`.
 // It returns a result message if successful, otherwise an error message.
 func convert(path string) string {
+	// open source file
 	in, err := os.Open(path)
 	if err != nil {
 		return err.Error()
 	}
 
+	// create destination file
 	outName := outPath(path, binExt)
 	out, err := os.Create(outName)
 	if err != nil {
 		return err.Error()
 	}
 
-	// convert in to out
+	// create a new Asm object
 	asmblr, err := asm.New(in)
 	if err != nil {
 		return err.Error()
 	}
+
+	// add pre-defined symbols
+	asmblr.DefineSymbols(preDefSymb)
+
+	// convert source file to binary file
 	if e := asmblr.Run(out); e != nil {
 		return e.Error()
 	}
